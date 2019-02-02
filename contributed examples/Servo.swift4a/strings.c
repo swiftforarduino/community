4e2ce6462bbcf37b54dd87d413805b6b17ee1ce3
Carl Peto <carl.peto@me.com>
2019-02-02 17:13:51 +0000

    - completed work on the servo library

---
+++ contributed examples/Servo.swift4a/strings.c
@@ -0,0 +1,5 @@
+#include <avr/pgmspace.h>
+
+const char Started0Mem[] PROGMEM = "Started"; const char * Started0 = &Started0Mem[0];
+
+const char Changed1Mem[] PROGMEM = "Changed servo angle to:"; const char * Changed1 = &Changed1Mem[0];
