d89700edf05f717a646965266ca5938e3180dfcb
Carl Peto <carl.peto@me.com>
2019-02-19 12:03:19 +0000

    - tidy up the stepper motor library and example code a bit, remove aborts

--- contributed examples/using peripherals/Stepper motor example.swift4a/main.swift
+++ contributed examples/using peripherals/Stepper motor example.swift4a/main.swift
@@ -2,19 +2,30 @@ import AVR
 
 typealias IntegerLiteralType = UInt8
 
-setupStepper()
+setupStepper(IN1: 8, IN2: 9, IN3: 10, IN4: 11)
 
 SetupSerial(baudRate: 9600)
 print(message: Put0)
 
 while(true) {
 
-  for i: UInt16 in 0..<4096 {
-    step(count: 1)
+  var i: UInt16 = 0
+  while i < 4096 {
+    singleStep()
     delay(microseconds: 800)
+    i = i &+ 1
   }
 
-  forward = !forward
+  reverseDirection()
   
   print(message: Reversing1)
 }
+
+// cellar... the code was this...
+// but it creates some internal aborts
+// which would probably never be triggered
+// but I'd rather not take the risk...
+//  for i: UInt16 in 0..<4096 {
+//    singleStep()
+//    delay(microseconds: 800)
+// }
--- contributed libraries/stepper-28BYJ-48.swift
+++ contributed libraries/stepper-28BYJ-48.swift
@@ -1,42 +1,49 @@
 import AVR
 
-let IN1 = 8
-let IN2 = 9
-let IN3 = 10
-let IN4 = 11
+private(set) var IN1 = 8
+private(set) var IN2 = 9
+private(set) var IN3 = 10
+private(set) var IN4 = 11
 
-var currentStep = 0
-var forward = true
+private var currentStep = 0
+private var forward = true
 
-func setupStepper() {
+func setDirection(forward forwardIn: Bool) {
+  forward = forwardIn
+}
+
+func reverseDirection() {
+  forward = !forward
+}
+
+func setupStepper(IN1 pin1: UInt8, IN2 pin2: UInt8, IN3 pin3: UInt8, IN4 pin4: UInt8) {
+  IN1 = pin1
+  IN2 = pin2
+  IN3 = pin3
+  IN4 = pin4
+
   pinMode(pin: IN1, mode: OUTPUT)
   pinMode(pin: IN2, mode: OUTPUT)
   pinMode(pin: IN3, mode: OUTPUT)
   pinMode(pin: IN4, mode: OUTPUT)
 }
 
-func updateStep() {
-
-  if forward {
-    currentStep = currentStep &+ 1
-    
-    if currentStep > 7 {
-      currentStep = 0
-    }
-  } else {
-    currentStep = currentStep &- 1
-
-    if currentStep == 255 {
-      currentStep = 7
-    }
-  }
+func singleStep() {
+  step()
+  setMotorCoils()
 }
 
 func step(count: UInt16) {
   var i: UInt16 = 0
   while i < count {
+    step()
+    setMotorCoils()
+    i = i &+ 1
+  }
+}
 
-  switch currentStep {
+private func setMotorCoils() {
+    switch currentStep {
       case 0:
         digitalWrite(pin: IN1, value: LOW)
         digitalWrite(pin: IN2, value: LOW)
@@ -89,8 +96,21 @@ func step(count: UInt16) {
         break
 
   }
+}
 
-  updateStep()
-  i = i + 1
+private func step() {
+
+  if forward {
+    currentStep = currentStep &+ 1
+    
+    if currentStep > 7 {
+      currentStep = 0
+    }
+  } else {
+    currentStep = currentStep &- 1
+
+    if currentStep == 255 {
+      currentStep = 7
+    }
   }
 }
