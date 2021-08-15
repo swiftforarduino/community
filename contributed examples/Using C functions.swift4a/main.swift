cc1d5644cd0531e55765e6ee4abd01282531fa81
Carl Peto <carl.peto@me.com>
2021-08-15 19:02:08 +0100

    add a pulse

--- contributed examples/Using C functions.swift4a/main.swift
+++ contributed examples/Using C functions.swift4a/main.swift
@@ -2,10 +2,16 @@ import AVR
 
 SetupSerial()
 
+pinMode(pin: D13, mode: OUTPUT)
+
 var r: Float = 9
 
 while(true) {
     r += 1
     print(sqrd(r))
     delay(ms: 1000)
+    digitalWrite(pin: 13, value: HIGH)
+    delay(milliseconds: 1000)
+    digitalWrite(pin: 13, value: LOW)
+    delay(milliseconds: 1000)
 }
